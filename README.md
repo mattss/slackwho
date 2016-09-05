8c8082bf163781287ee6286714672fc89052f186
Matthew Sital-Singh <mattss@netsight.co.uk>
2016-09-05 10:04:22 +0100

    Initial working script

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # slackwho
-At-a-glance presence checker for Slack
+At-a-glance multi-account presence checker for Slack
+
+Designed for use with geektool or similar.
+
+## Config
+
+ * Create an accounts.json, mapping account names to api tokens:
+
+
+    {"slack account 1": "<token-for-account-1>",
+     "slack account 2": "<token-for-account-2>"
+     }
+
+    (Generate personal API tokens at https://api.slack.com/web#authentication)
+
+ * Create a virtualenv and activate
+
+ * Install requirements
+
+    pip install -r requirements.txt
+
+## Run
+
+    python slackwho.py
+
+## Output
+
+All online users, followed by the account(s) they are active on.
+
+    user1 (accountlist)
+    user2 (accountlist)
+    user3 (accountlist)
---
+++ slackwho.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import json
+import sys
+from collections import defaultdict
+
+from slackclient import SlackClient
+
+CONFIG_FILENAME = 'accounts.json'
+
+try:
+    channels = json.loads(open(CONFIG_FILENAME).read())
+except FileNotFoundError:
+    print('Error reading config from {}'.format(CONFIG_FILENAME))
+    print('File not found')
+    sys.exit(1)
+except json.decoder.JSONDecodeError as jse:
+    print('Error reading config from {}'.format(CONFIG_FILENAME))
+    print(jse)
+    sys.exit(1)
+
+online = defaultdict(list)
+for account_name, token in channels.items():
+    sc = SlackClient(token)
+    users = sc.api_call("users.list")
+    for user in users['members']:
+        userid = user['id']
+        username = user['name']
+        presence = sc.api_call('users.getPresence', user=userid)
+        if presence['presence'] == 'away':
+            continue
+        online[username].append(account_name)
+
+for username, channel_list in sorted(online.items()):
+    print('{} ({})'.format(username, ', '.join(channel_list)))
