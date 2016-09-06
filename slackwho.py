622ae6d8acd2b5f8a41f5731b052c1c8fc4182fa
Matthew Sital-Singh <mattss@netsight.co.uk>
2016-09-06 11:24:08 +0100

    Add DND info

--- slackwho.py
+++ slackwho.py
@@ -2,6 +2,7 @@
 import json
 import sys
 import os
+from datetime import datetime
 from collections import defaultdict
 from requests.exceptions import ConnectionError
 
@@ -22,6 +23,7 @@ except json.decoder.JSONDecodeError as jse:
     print(jse)
     sys.exit(1)
 
+now = datetime.now()
 online = defaultdict(list)
 for account_name, token in channels.items():
     sc = SlackClient(token)
@@ -36,11 +38,17 @@ for account_name, token in channels.items():
         presence = sc.api_call('users.getPresence', user=userid)
         if presence['presence'] == 'away':
             continue
+        dnd = sc.api_call('dnd.info', user=userid)
+        next_dnd_end = datetime.fromtimestamp(dnd['next_dnd_end_ts'])
+        next_dnd_start = datetime.fromtimestamp(dnd['next_dnd_start_ts'])
+        if next_dnd_start < now < next_dnd_end:
+            username += '-dnd'
         online[username].append(account_name)
 
 print(':: slackwho ::')
 max_username_length = len(max(online.keys(), key=len))
 for username, channel_list in sorted(online.items()):
+    # bold username, tab, channel list
     print('\033[1m{}\033[0m\t{}'.format(
         username.ljust(max_username_length, ' '),
         ', '.join(sorted(channel_list))
